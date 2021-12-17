4a9175fc73681dceec39ad614760c521714b9d53
gianmartind <63358478+gianmartind@users.noreply.github.com>
2021-12-17 18:57:39 +0700

    many

--- load_data.py
+++ load_data.py
@@ -5,18 +5,41 @@ Created on Thu Dec 16 22:17:26 2021
 @author: gianm
 """
 
-#%%
+#%% import library
 import dataset
 import pandas as pd
 
-#%%
-github_data = dataset.download_github_data([3, 4])
+#%% download data
+github_data = dataset.download_github_data([0, 3])
 
 #%% owid-covid-data
 df_owid = github_data['owid-covid-data.csv']
 
 #pilih hanya kolom yang digunakan
-df_owid = df_owid[['continent', 'location', 'date', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths']]
+df_owid = df_owid[['continent', 'location', 'date', 'new_cases', 'new_deaths']]
 
-#ubah nan menjadi 0
+#ubah nilai nan menjadi 0
 df_owid.fillna(0, inplace=True)
+
+#hapus baris dengan continent == 0
+df_owid = df_owid[df_owid['continent'] != 0]
+
+#gunakan hanya tahun 2021
+df_owid = df_owid[df_owid['date'].str.startswith('2021')]
+
+#%% vaccine-by-manufacturers
+df_vacc = github_data['vaccinations.csv']
+
+#pilih hanya kolom yang digunakan
+df_vacc = df_vacc[['location', 'date', 'daily_vaccinations']]
+
+#ubah nilai nan menjadi 0
+df_vacc.fillna(0, inplace=True)
+
+#%% load to hdfs
+cleaned_datasets = dict()
+cleaned_datasets['owid-covid-data.csv'] = df_owid
+cleaned_datasets['vaccinations.csv'] = df_vacc
+
+dataset.load_to_hdfs(cleaned_datasets)
+
